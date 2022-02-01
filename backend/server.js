d421fdba156c4f511c546069b0c01a0d1fa44b16
devydk <78767451+devydk@users.noreply.github.com>
2022-02-01 16:23:09 -0300

    commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+// modules 
+const express =  require("express");
+const mongoose =  require("mongoose");
+const bobyParser =  require("body-parser");
+const cors = require("cors");
+
+// routes
+
+//  middlewares
+
+// config
+const dbName = "partytimeb";
+const port = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+//atrelar as rotas no express 
+
+// conexao mongodb
+mongoose.connect(
+  `mongodb://localhost/${dbName}`,
+  {
+    useNewUrlParser: true,
+    //useFindAndModify: false,
+    //useUnifiedTopalogy: true
+  }
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Rota teste1"});
+
+});
+
+app.listen(port, () => {
+  console.log(`o backend está rodando na porta ${port}`);
+})
